6cb43d7e1aabb9434ce86e2e14571015bf068cca
Joost <joostvermeij@gmail.com>
2024-07-31 14:10:23 +0200

    env fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import {AfterViewInit, Component} from '@angular/core';
-import {Env} from './env';
 import {environment} from "../environments/environment";
-import {process} from 'angular-server-side-configuration/process';
-import {HttpClient} from "@angular/common/http";
-import {map} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,20 +10,10 @@ export class AppComponent implements AfterViewInit {
   title = 'test-app';
   public environmentNew: any = {MESSAGE: 'init', POD_NAME: 'init'};
 
-  constructor(public http: HttpClient) {
-  }
-
   ngAfterViewInit(): void {
     this.environmentNew = environment;
     console.log("env", process, process.env);
-    console.log("environment", environment, environment.MESSAGE, environment.POD_NAME)
-
-    this.http.get('assets/json/runtime.json')
-      .pipe(map(result =>
-        {
-           console.log(result);
-        }
-      )).subscribe();
+    console.log("environment", environment)
   }
 
 }
